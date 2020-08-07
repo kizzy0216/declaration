6343a272a7bb86614557e8a0ef6dadcb0faadb31
Nick Dandakis <nickdandakis@gmail.com>
2020-08-07 19:49:14 -0400

    Release

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -45,12 +45,12 @@ export default {
     bundleIdentifier: process.env.APPLICATION_PACKAGE_ID,
     // irrelevant to version number, this is a build identifier
     // just increment per build of a version
-    buildNumber: '5',
+    buildNumber: '6',
   },
   android: {
     package: process.env.APPLICATION_PACKAGE_ID,
     // irrelevant to version number, this is a build identifier
     // just increment per build of a version
-    versionCode: 5,
+    versionCode: 6,
   },
 }
